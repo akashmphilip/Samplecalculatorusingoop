e5ccb353a7bfc9142c22ae1906fc8da284ef472f
akashmphilip <42133843+akashmphilip@users.noreply.github.com>
2022-04-04 08:54:38 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<form action="calcs.php" method="post">
+			<input type="text" name="num1" value="">
+			<input type="text" name="num2" value="">
+			<select name="cal">
+				<option value="add">Add</option>
+				<option value="sub">Subtract</option>
+				<option value="mul">Multiply</option>
+				<option value="div">Division</option>
+			</select>
+			<button type="submit">Calculate</button>
+		</form>
+	</body>
+</html>
